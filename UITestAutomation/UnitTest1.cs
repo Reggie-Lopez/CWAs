af6104969abf2c782a6ad0a0611e35321bb5c5eb
Jeff Furois <JFurois@eimagine.com>
2022-12-06 12:16:53 -0500

    intial check-in

---
+++ UITestAutomation/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UITestAutomation
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
