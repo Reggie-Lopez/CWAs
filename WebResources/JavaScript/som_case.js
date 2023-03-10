940724c773c4ae0533a3f1a9acf08862dea78fb5
Jeff Furois <JFurois@eimagine.com>
2023-03-10 17:39:24 -0500

    added logic for the form toggle

--- WebResources/JavaScript/som_case.js
+++ WebResources/JavaScript/som_case.js
@@ -1,3 +1,59 @@
 function case_OnLoad(context) {
-    console.log('loaded');
+    console.log('form onload called');
+
+    //run form logic
+    CheckCorrectForm(context);   
+}
+
+
+function case_OnSave(context) {
+    console.log('form onsave called');
+    var formContext = context.getFormContext();
+}
+
+
+function CheckCorrectForm(context) {
+    //get current form
+    var formContext = context.getFormContext();
+    var formSaveType = formContext.ui.getFormType();
+    //get current form
+    var formItem = formContext.ui.formSelector.getCurrentItem();
+    //check if the record has been saved and it's STILL on the entry form. redirect to correct form
+    if (formItem.getLabel() == "Entry" && formSaveType != 1) {
+        ChangeForm(formContext);
+    }
+}
+
+
+function ChangeForm(formContext) {
+    //get casetype optionset
+    var caseType = formContext.getAttribute("casetypecode");
+
+    if (caseType != null) {
+        //get text value of optionset
+        var formNameToChangeTo = caseType.getText();
+        //get all forms
+        var allForms = formContext.ui.formSelector.items.get();
+        //loop through all forms and set to the form that was selected in the casetype optionset
+        var newForm;
+        for (var f in allForms) {
+            var form = allForms[f];
+            var formName = form.getLabel().toLowerCase();
+            //check if the form is the same as the option selected in the optionset
+            if (formName == formNameToChangeTo.toLowerCase()) {
+                //set var and exit loop
+                newForm = form;
+                break;
+            }
+        }
+        if (!newForm) {
+            console.log("No form created for the optionset value selected.");
+            alert("No form created for the optionset value selected.");
+            return;
+        }
+
+        //change form
+        newForm.navigate();
+        return true;                  
+    }
 }
