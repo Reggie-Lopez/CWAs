cf01faff9a7f17ffd7b1f61bf7e77f1f1a4ec207
Jeff Furois <JFurois@eimagine.com>
2023-03-14 08:59:10 -0400

    updated logic to toggle to correct form based on lookup. and show a notification if the form doesnt exist

--- WebResources/JavaScript/som_case.js
+++ WebResources/JavaScript/som_case.js
@@ -2,77 +2,81 @@ function case_OnLoad(context) {
     console.log('form onload called');
 
     //run form logic
-    CheckCorrectForm(context);   
+    ChangeToCorrectForm(context);   
 }
 
 
 function case_OnSave(context) {
     console.log('form onsave called');
     var formContext = context.getFormContext();
+
+    //confirm there is an actual form named what is in the Case Type lookup
+    var formCheck = GetForm(context);
+    //if no form is returned then add notification to record
+    if (!formCheck) {
+        var errorMsg = "Form does not exist for the Case Type selected.";
+        formContext.ui.setFormNotification(errorMsg, "ERROR", "NoFormExists");
+        context.getEventArgs().preventDefault();
+        console.log(errorMsg);
+    }
 }
 
 
-function CheckCorrectForm(context) {
+function ChangeToCorrectForm(context) {
     //get current form
     var formContext = context.getFormContext();
-    var formSaveType = formContext.ui.getFormType();
+    var formContext = context.getFormContext();
+
     //get current form
-    //var formItem = formContext.ui.formSelector.getCurrentItem();
+    var formSaveType = formContext.ui.getFormType();    
+    var formItem = formContext.ui.formSelector.getCurrentItem();   
 
-    //get case type lookup 
-    var caseType = formContext.getAttribute("som_casetype");
-    if (caseType != null) {
-        var caseTypeLookup = caseType.getValue();
-        var caseTypeLookupName = caseTypeLookup[0].name;
+    var formToChangeTo;
 
-        //check if the record has been saved and it's STILL on the entry form. redirect to correct form
-        if (caseTypeLookupName == "Entry" && formSaveType != 1) {
-            ChangeForm(formContext);
-        }
-
-        //check if the current form is NOT Entry and it's a new record
-        if (caseTypeLookupName != "Entry" && formSaveType == 1) {
-            ChangeForm(formContext, true);
-        }
+    //if the current form is Entry but the record is already saved then change to appropriate form
+    if (formItem.getLabel() == "Entry" && formSaveType != 1) {
+        formToChangeTo = GetForm(context);
     }
+
+    //if the current form is NOT Entry and the record has not been saved yet
+    if (formItem.getLabel() != "Entry" && formSaveType == 1) {
+        formToChangeTo = GetForm(context, "Entry");
+    }
+
+    if (formToChangeTo) {
+        //change form
+        formToChangeTo.navigate();
+        return true;
+    }    
 }
 
 
-function ChangeForm(formContext, changeToEntryForm) {
-    //get casetype optionset
-    //var caseType = formContext.getAttribute("casetypecode");
+function GetForm(context, formOverrride) {
+    var formContext = context.getFormContext();
+
+    //override if the ChangeToEntryForm is true
+    if (formOverrride) { caseTypeLookupName = "Entry"; }
 
     //get case type lookup 
     var caseType = formContext.getAttribute("som_casetype");
-    if (caseType != null) {
-        var caseTypeLookup = caseType.getValue();
-        //get text of lookup
-        var caseTypeLookupName = caseTypeLookup[0].name;
-        //override if the ChangeToEntryForm is true
-        if (changeToEntryForm) { caseTypeLookupName = "Entry"; }
-             
-        //get all forms
-        var allForms = formContext.ui.formSelector.items.get();
-        //loop through all forms and set to the form that was selected in the casetype optionset
-        var newForm;
-        for (var f in allForms) {
-            var form = allForms[f];
-            var formName = form.getLabel().toLowerCase();
-            //check if the form is the same as the option selected in the optionset
-            if (formName == caseTypeLookupName) {
-                //set var and exit loop
-                newForm = form;
-                break;
-            }
-        }
-        if (!newForm) {
-            console.log("No form created for the case type record selected.");
-            alert("No form created for the case type record selected.");
-            return;
-        }
-
-        //change form
-        newForm.navigate();
-        return true;                  
+    var caseTypeLookup = caseType.getValue();
+    if (caseTypeLookup != null) {
+        //get case type lookup name
+        caseTypeLookupName = caseTypeLookup[0].name;
     }
+    //get all forms
+    var allForms = formContext.ui.formSelector.items.get();
+    //loop through all forms and set to the form that was selected in the casetype lookup
+    for (var f in allForms) {
+        var form = allForms[f];
+        var formName = form.getLabel().toLowerCase();
+        //check if the form is the same as the value in the lookup
+        if (formName == caseTypeLookupName.toLowerCase()) {
+            //return form
+            return form;
+        }
+    }
+    
+    //if it can't find the form then return null
+    return null;
 }
