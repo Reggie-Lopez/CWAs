d2ea89f1a6bba17a202579b2da1defdc6e1de818
Jeff Furois <JFurois@eimagine.com>
2023-04-11 16:36:39 -0400

    initial check-in of transaction js lib

---
+++ WebResources/JavaScript/som_transaction.js
@@ -0,0 +1,13 @@
+function transaction_OnLoad(context) {
+    console.log('form onload called');
+
+
+}
+
+
+function transaction_OnSave(context) {
+    console.log('form onsave called');
+    var formContext = context.getFormContext();
+
+    
+}
