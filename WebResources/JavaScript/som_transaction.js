7af0e5ab486dc07381df12c607026b09b8f6e434
Jeff Furois <JFurois@eimagine.com>
2023-04-11 16:41:12 -0400

    added logic to set the Position field on the Transaction record onload

--- WebResources/JavaScript/som_transaction.js
+++ WebResources/JavaScript/som_transaction.js
@@ -1,7 +1,12 @@
-function transaction_OnLoad(context) {
+function transaction_OnLoad(executionContext) {
     console.log('form onload called');
 
+    var formContext = executionContext.getFormContext();
 
+    formContext.getAttribute('som_secondarycategory').addOnChange(secondarycategory_OnChange);
+
+    //default Position field
+    setPosition(executionContext);
 }
 
 
@@ -10,4 +15,27 @@ function transaction_OnSave(context) {
     var formContext = context.getFormContext();
 
     
+}
+
+function setPosition(executionContext) {
+    var formContext = executionContext.getFormContext();
+
+    var position = formContext.getAttribute('som_position').getValue()?.[0]?.id;
+    if (!!position) {
+        return;
+    }
+
+    Xrm.WebApi.retrieveRecord('systemuser', formContext.context.getUserId(), '?$expand=positionid($select=positionid)').then(result => {
+        var positionid = [
+            {
+                id: result['_positionid_value'],
+                name: result["_positionid_value@OData.Community.Display.V1.FormattedValue"],
+                entityType: result["_positionid_value@Microsoft.Dynamics.CRM.lookuplogicalname"]
+            }
+        ];
+
+        formContext.getAttribute('som_position').setValue(positionid);
+    }, error => {
+        console.log(error);
+    });
 }
