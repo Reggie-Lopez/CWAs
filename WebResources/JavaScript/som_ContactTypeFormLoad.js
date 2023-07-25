0389557fb26c6e3947c16e06602b2f9c2304353a
Paspu, Adithya (DTMB-Contractor) <PaspuA@michigan.gov>
2023-07-25 01:29:07 -0400

    Checkin in All JS Code Files

---
+++ WebResources/JavaScript/som_ContactTypeFormLoad.js
@@ -0,0 +1,58 @@
+function contact_OnLoad(context) {
+    console.log('form onload called');
+
+    //run form logic
+    ChangeToCorrectForm(context);   
+}
+
+
+
+
+function ChangeToCorrectForm(context) {
+    //get current form
+    var formContext = context.getFormContext();
+
+    //get current form
+    var formSaveType = formContext.ui.getFormType();    
+    var formItem = formContext.ui.formSelector.getCurrentItem();   
+
+    var formToChangeTo;
+    var contactType = formContext.getAttribute("som_contacttype").getText();
+
+    if (contactType == 'Dependent') {
+             if (formItem.getLabel() != "Dependent") {
+            formToChangeTo = GetForm(context, "Dependent");
+        }
+    } else {
+        if (formItem.getLabel() != "MCSC") {
+            formToChangeTo = GetForm(context, "MCSC");
+        }
+    }
+
+
+    if (formToChangeTo) {
+        //change form
+        formToChangeTo.navigate();
+        return true;
+    }    
+}
+
+
+function GetForm(context, formOverrride) {
+ var formContext = context.getFormContext();
+    //get all forms
+    var allForms = formContext.ui.formSelector.items.get();
+    //loop through all forms and set to the form that was selected in the contacttype lookup
+    for (var f in allForms) {
+        var form = allForms[f];
+        var formName = form.getLabel().toLowerCase();
+        //check if the form is the same as the value in the lookup
+        if (formName == formOverrride.toLowerCase()) {
+            //return form
+            return form;
+        }
+    }
+    
+    //if it can't find the form then return null
+    return null;
+}
---
+++ WebResources/JavaScript/som_FilterLeaveDenialReasonsbyLeaveType.js
@@ -0,0 +1,25 @@
+function AddCustomFilter(executionContext) {
+    var formContext = executionContext.getFormContext();
+
+    //Check if the field is not null
+    if (formContext.getAttribute("som_leavetype").getValue() != null) {
+        //Apply Search on lookup field OnClick 
+        formContext.getControl("som_leavedenialreason1").addPreSearch(function () {
+            var leaveType = formContext.getAttribute("som_leavetype").getValue();
+            //Apply the filter condition
+            var filter = "<filter type='and'><condition attribute='som_leavetype' operator='eq' value='" + leaveType + "' /></filter>";
+            //Populate the filter values into lookup field
+            formContext.getControl("som_leavedenialreason1").addCustomFilter(filter);
+             });
+
+        //Apply Search on lookup field OnClick 
+        formContext.getControl("som_leavedenialreason2").addPreSearch(function () {
+            var leaveType = formContext.getAttribute("som_leavetype").getValue();
+            //Apply the filter condition
+            var filter = "<filter type='and'><condition attribute='som_leavetype' operator='eq' value='" + leaveType + "' /></filter>";
+            //Populate the filter values into lookup field
+            formContext.getControl("som_leavedenialreason2").addCustomFilter(filter);
+        });
+
+    }
+}
