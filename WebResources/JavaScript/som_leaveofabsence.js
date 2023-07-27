d2fefe13fbd756247d12369e171e2439c3b3efdd
Jeff Furois <JFurois@eimagine.com>
2023-07-27 17:43:15 -0400

    made changed to ensure the lookup is only showing the right values based on the UX use cases

--- WebResources/JavaScript/som_leaveofabsence.js
+++ WebResources/JavaScript/som_leaveofabsence.js
@@ -1,25 +1,43 @@
+function leaveofabsence_OnLoad(context) {
+    console.log('form onload called');
+    var formContext = context.getFormContext();
+
+    formContext.getAttribute('som_leavetype').addOnChange(AddCustomFilter);
+    formContext.getAttribute('som_leavetype').addOnChange(ClearLeaveDenialReasons);
+
+    AddCustomFilter(context);
+}
+
+
+function leaveofabsence_OnSave(context) {
+    console.log('form onsave called');
+    
+}
+
+
+function ClearLeaveDenialReasons(context) {
+    var formContext = context.getFormContext();
+    formContext.getAttribute("som_leavedenialreason1").setValue(null);
+    formContext.getAttribute("som_leavedenialreason2").setValue(null);
+}
+
+
 function AddCustomFilter(executionContext) {
     var formContext = executionContext.getFormContext();
-  formContext.getAttribute('som_leavetype').addOnChange(AddCustomFilter);
-    //Check if the field is not null
-    if (formContext.getAttribute("som_leavetype").getValue() != null) {
-        //Apply Search on lookup field OnClick 
-        formContext.getControl("som_leavedenialreason1").addPreSearch(function () {
-            var leaveType = formContext.getAttribute("som_leavetype").getValue();
-            //Apply the filter condition
-            var filter = "<filter type='and'><condition attribute='som_leavetype' operator='eq' value='" + leaveType + "' /></filter>";
-            //Populate the filter values into lookup field
-            formContext.getControl("som_leavedenialreason1").addCustomFilter(filter);
-             });
+    formContext.getControl("som_leavedenialreason1").addPreSearch(function () {
+        var leaveType = formContext.getAttribute("som_leavetype").getValue();
+        //Apply the filter condition
+        var filter = "<filter type='and'><condition attribute='som_leavetype' operator='eq' value='" + leaveType + "' /></filter>";
+        //Populate the filter values into lookup field
+        formContext.getControl("som_leavedenialreason1").addCustomFilter(filter);
+    });
 
-        //Apply Search on lookup field OnClick 
-        formContext.getControl("som_leavedenialreason2").addPreSearch(function () {
-            var leaveType = formContext.getAttribute("som_leavetype").getValue();
-            //Apply the filter condition
-            var filter = "<filter type='and'><condition attribute='som_leavetype' operator='eq' value='" + leaveType + "' /></filter>";
-            //Populate the filter values into lookup field
-            formContext.getControl("som_leavedenialreason2").addCustomFilter(filter);
-        });
+    formContext.getControl("som_leavedenialreason2").addPreSearch(function () {
+        var leaveType = formContext.getAttribute("som_leavetype").getValue();
+        //Apply the filter condition
+        var filter = "<filter type='and'><condition attribute='som_leavetype' operator='eq' value='" + leaveType + "' /></filter>";
+        //Populate the filter values into lookup field
+        formContext.getControl("som_leavedenialreason2").addCustomFilter(filter);
+    });
 
-    }
 }
