808f4999d4e2f1fe3b48294c368ceaf13ae1988a
Reggie <reggie@lopus.dev>
2023-08-09 14:45:22 -0500

    Checking In Initial ( USER STORY 105713, USER STORY 105712)
    Report Headers, and formatting feedback from Jeff Furois not yet implemented.

--- CustomAssemblies/MCSC.Plugin.AssignCaseToCaseWorker/AssignCaseToCaseWorker.cs
+++ CustomAssemblies/MCSC.Plugin.AssignCaseToCaseWorker/AssignCaseToCaseWorker.cs
@@ -53,62 +53,72 @@ namespace MCSC.Plugin.AssignCaseToCaseWorker
 
                 if (!caseTypeNames.Contains(caseTypeName)) return;
 
-                var caseWorkerCaseQuery = new QueryExpression("systemuser")
+
+                var caseAssignmentQuery = new QueryExpression("som_caseassignment")
                 {
-                    ColumnSet = new ColumnSet("som_capacity"),
-                    LinkEntities =
+                    ColumnSet = new ColumnSet("som_assigneeid"),
+                    Criteria = new FilterExpression(LogicalOperator.And)
                     {
-                        new LinkEntity("systemuser", "som_caseassignment", "systemuserid", "som_assigneeid", JoinOperator.Inner)
-                        {
-                            Columns = new ColumnSet(false),
-                            LinkCriteria = new FilterExpression(LogicalOperator.And)
-                            {
-                                Conditions =
-                                {
-                                    new ConditionExpression("som_processlevel", ConditionOperator.Equal, processLevel),
-                                    new ConditionExpression("som_casetype", ConditionOperator.Equal, caseTypeRef.Id),
-                                }
-                            }
-                        },
-                        new LinkEntity("systemuser", "incident", "systemuserid", "ownerid", JoinOperator.LeftOuter)
-                        {
-                            Columns = new ColumnSet(false),
-                            LinkCriteria = new FilterExpression(LogicalOperator.And)
-                            {
-                                Conditions =
-                                {
-                                    new ConditionExpression("statecode", ConditionOperator.Equal, 0)
-                                }
-                            }
-                        }
+                        Conditions =
+                {
+                    new ConditionExpression("som_processlevel", ConditionOperator.Equal, processLevel),
+                    new ConditionExpression("som_casetype", ConditionOperator.Equal, caseTypeRef.Id),
+                }
                     }
                 };
-                var caseWorkerCases = service.RetrieveMultiple(caseWorkerCaseQuery)?.Entities;
 
-                var caseWorkers = caseWorkerCases?.GroupBy(x => x.Id)?.Select(x => new
+                var caseAssignments = service.RetrieveMultiple(caseAssignmentQuery)?.Entities;
+
+                // If only one case assignment record is found, assign the case to the assignee
+                if (caseAssignments != null && caseAssignments.Count == 1)
                 {
-                    EntityObject = x.FirstOrDefault(),
-                    CapacityAvailable = x.FirstOrDefault().GetAttributeValue<int>("som_capacity") - x.Count(),
-                });
+                    var caseUpdate = new Entity(target.LogicalName, target.Id);
+                    caseUpdate["ownerid"] = caseAssignments[0].GetAttributeValue<EntityReference>("som_assigneeid");
+                    service.Update(caseUpdate);
 
-                var caseWorker = caseWorkers?.OrderByDescending(x => x.CapacityAvailable)?.Select(x => x.EntityObject)?.FirstOrDefault();
+                    return;
+                }
 
-                var caseUpdate = new Entity(target.LogicalName, target.Id);
-                caseUpdate["ownerid"] = new EntityReference(caseWorker.LogicalName, caseWorker.Id);
-                service.Update(caseUpdate);
 
+
+                // If more than one case assignment record is found, check if any of them is a team
+                var teamAssignments = caseAssignments?.Where(x => x.GetAttributeValue<EntityReference>("som_assigneeid").LogicalName == "team");
+
+                if (teamAssignments != null && teamAssignments.Any())
+                {
+                    // If a team is found, assign the case to the first team
+                    var caseUpdate = new Entity(target.LogicalName, target.Id);
+                    caseUpdate["ownerid"] = teamAssignments.First().GetAttributeValue<EntityReference>("som_assigneeid");
+                    service.Update(caseUpdate);
+                }
+                else
+                {
+                    // If no team is found, use the existing capacity logic to assign the case to a user
+                    var caseWorkerCases = caseAssignments?.Where(x => x.GetAttributeValue<EntityReference>("som_assigneeid").LogicalName == "systemuser");
+
+                    var caseWorkers = caseWorkerCases?.GroupBy(x => x.Id)?.Select(x => new
+                    {
+                        EntityObject = x.FirstOrDefault(),
+                        CapacityAvailable = x.FirstOrDefault().GetAttributeValue<int>("som_capacity") - x.Count(),
+                    });
+
+                    var caseWorker = caseWorkers?.OrderByDescending(x => x.CapacityAvailable)?.Select(x => x.EntityObject)?.FirstOrDefault();
+
+                    if (caseWorker != null)
+                    {
+                        var caseUpdate = new Entity(target.LogicalName, target.Id);
+                        caseUpdate["ownerid"] = new EntityReference(caseWorker.LogicalName, caseWorker.Id);
+                        service.Update(caseUpdate);
+                    }
+                }
             }
             catch (Exception ex)
             {
-                service.Create(new Entity("som_logentry")
-                {
-                    ["som_source"] = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name,
-                    ["som_name"] = ex.Message,
-                    ["som_details"] = ex.StackTrace,
-                    ["som_severity"] = new OptionSetValue(LOG_ENTRY_SEVERITY_ERROR),
-                    ["som_recordlogicalname"] = $"{context?.PrimaryEntityName}",
-                    ["som_recordid"] = $"{context?.PrimaryEntityId}",
-                });
+                // Write exception details to the plugin trace log
+                _trace.Trace($"Exception: {ex.Message} \n{ex.StackTrace}");
+
+                // Re-throw the exception to ensure it's caught by Dynamics 365
+                throw;
             }
         }
     }
